84e1624ea3de92c76f5f1aa4adc1b08c65de95ef
isurs93 <loadofcastle@gmail.com>
2020-07-17 20:04:19 +0900

    Add List and heart Icon

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,95 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-//  final _biggerFont = TextStyle(fontSize: 18.0);
-//
-//  Widget _buildSuggestions() {
-//    return ListView.builder(
-//        padding: EdgeInsets.all(16.0),
-//        itemBuilder: /*1*/ (context, i) {
-//          if (i.isOdd) return Divider(); /*2*/
-//
-//          final index = i ~/ 2; /*3*/
-//          if (index >= _suggestions.length) {
-//            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-//          }
-//          return _buildRow(_suggestions[index]);
-//        });
-//  }
+  // const는 빌드 하면서 미리 계산
+  // final은 빌드 완료 후 불러와질때 계산
+  final _saved = Set<WordPair>();     // NEW
+  final _biggerFont = const TextStyle(fontSize: 18.0); // fontSize는 Double로
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+//    final wordPair = WordPair.random();
+//    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, // ...to here.
+      ),
+    );
+  }
+
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return Divider();
+        // Divider도 widget이다... 리스트의 항목을 선으로 나누는것.
+        /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return _buildRow(_suggestions[index]);
+      });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);  // NEW
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
   }
 }
 
@@ -40,17 +109,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          //child: Text('이곳에 문자열을 넣으면 출력한다!!!'),
-          //child: Text(wordPair.asPascalCase),
-          child: RandomWords()
-        ),
+//      title: 'Welcome to Flutter',
+//      home: Scaffold(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.white,
       ),
+      home: RandomWords(),
+//        appBar: AppBar(
+//          title: Text('Welcome to Flutter'),
+//        ),
+//        body: Center(
+//          //child: Text('이곳에 문자열을 넣으면 출력한다!!!'),
+//          //child: Text(wordPair.asPascalCase),
+//          child: RandomWords()
+//        ),
+//      ),
     );
   }
 }
